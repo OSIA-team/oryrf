e45458606ca06d7ee6dabaf826f0551626d860b6
Patriksafar <patrik.safar@educanet.cz>
2018-02-28 20:05:42 +0100

    add login and register forms

--- web/templates/header.php
+++ web/templates/header.php
@@ -3,5 +3,72 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 <!-- Latest compiled and minified CSS -->
 <nav class="user">
-<a href="#">Přihlásit se</a> nebo <a href="#">registrovat</a>
+<a class="waves-effect waves-light modal-trigger" href="#login">Přihlásit se</a> nebo <a class="waves-effect waves-light modal-trigger" href="#register">registrovat</a>
 </nav>
+
+<!-- Modal Structure -->
+  <div id="login" class="modal grey lighten-4">
+   <div class="modal-content row">
+     <img src="pict/logo_bezpozadi.png" alt="branding logo" class="brand">
+     <h5>Přihlašte se</h5>
+     <form class="" action="" method="post">
+       <div class='row'>
+         <div class='input-field col s12'>
+           <input class='validate' type='email' name='email' id='email' />
+           <label for='email'>Váš email</label>
+         </div>
+       </div>
+
+       <div class='row'>
+         <div class='input-field col s12'>
+           <input class='validate' type='password' name='password' id='password' />
+           <label for='password'>Vaše heslo</label>
+         </div>
+         <label style='float: right;'>
+          <a class='forgot' href='#!'><b>Zapomněl jsem</b></a>
+        </label>
+       </div>
+       <input type="submit" class="btn amber darken-2 center" name="login" value="Přihlásit">
+     </form>
+   </div>
+ </div>
+
+
+ <!-- Modal Structure -->
+   <div id="register" class="modal grey lighten-4">
+    <div class="modal-content row">
+      <img src="pict/logo_bezpozadi.png" alt="branding logo" class="brand">
+      <h5>Registrovat se</h5>
+      <form class="" action="" method="post">
+        <div class='row'>
+          <div class='input-field col s12'>
+            <input class='validate' type='email' name='email' id='email-reg' />
+            <label for='email-reg'>Váš email</label>
+          </div>
+
+          <div class='input-field col s12'>
+            <input class='validate' type='password' name='password' id='password-reg' />
+            <label for='password-reg'>Vaše heslo</label>
+          </div>
+          <h6>Kontaktní údaje</h6>
+          <div class='input-field col s6'>
+            <input class='validate' type='text' name='jmeno' id='jmeno' />
+            <label for='jmeno'>Jméno</label>
+          </div>
+          <div class='input-field col s6'>
+            <input class='validate' type='text' name='prijmeni' id='prijmeni' />
+            <label for='prijmeni'>Příjmení</label>
+          </div>
+          <div class='input-field col s12'>
+            <input class='validate' type='text' name='adresa' id='address' />
+            <label for='adresa'>Adresa pro doručení</label>
+          </div>
+          <div class='input-field col s12'>
+            <input class='validate' type='text' name='telefon' id='telefon' value="+420"/>
+            <label for='telefon'>Telefon</label>
+          </div>
+        </div>
+        <input type="submit" class="btn amber darken-2 center" name="login" value="Registrovat se">
+      </form>
+    </div>
+  </div>
