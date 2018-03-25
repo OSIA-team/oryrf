cb9c34fbec586b3b270ddb5af2fa87ffbde7215f
Patriksafar <patrik.safar@educanet.cz>
2018-03-25 12:15:37 +0200

    add new footer

--- web/templates/footer.php
+++ web/templates/footer.php
@@ -1,4 +1,30 @@
 </section>
+<footer class="page-footer grey darken-3">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Kontakt</h5>
+              <p class="grey-text text-lighten-4">Kontakt ? </p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Důležité odkazy</h5>
+              <ul>
+                <li><a class="grey-text text-lighten-3" href="?page=menu">Menu</a></li>
+                <li><a class="grey-text text-lighten-3" href="?page=kontakt">Kde nás najdete</a></li>
+                <li><a class="grey-text text-lighten-3" href="?page=onas">O nás</a></li>
+                <li><a class="grey-text text-lighten-3 modal-trigger" href="#login">Přihlášení</a></li>
+                <li><a class="grey-text text-lighten-3 modal-trigger" href="#register">Registrace</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+          © 2018 Copyright Bel3s
+          <a class="grey-text text-lighten-4 right" href="#!">Made with love by OSIA</a>
+          </div>
+        </div>
+      </footer>
   <script type="text/javascript">
       $(document).ready(function(){
           // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
