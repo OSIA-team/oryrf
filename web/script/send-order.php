625acead965d7915fa3a530cde2154adcb4faa93
Patriksafar <patrik.safar@educanet.cz>
2018-03-25 21:37:31 +0200

    Merge branch 'master' of https://github.com/OSIA-team/bel3s

--- web/script/send-order.php
+++ web/script/send-order.php
@@ -7,20 +7,16 @@ foreach (glob("../model/*.php") as $filename)
 }
 \core\core::$configFile = require_once '../config.php';
 $kosikClass = new database\kosik();
-
     if (is_array($_POST['jidlo_id'])){
         foreach ($_POST['jidlo_id'] as $key => $value){
-            $result = $kosikClass->addInKosik($_POST['jidlo_id'][$key], 1);
+            $result = $kosikClass->addInKosik($value, 1);
         }
     } else {
-        $pocet = $_POST['quntity-1'];
+        @$pocet = $_POST['quntity-1'];
         $jidlo_id = $_POST['jidlo_id'];
         $result = $kosikClass->addInKosik($jidlo_id, $pocet);
     }
-
     $newPocet = $kosikClass->getPocet();
     $newCena = $kosikClass->getCena();
-
     echo json_encode(array("cena" => $newCena, 'pocet' => $newPocet));
-
 ?>
