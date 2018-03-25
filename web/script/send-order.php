ec6dbcdf278fa4dade256d1cd957a1104c29f486
TofaVoe <k.kosut@gmail.com>
2018-03-25 21:27:53 +0200

    BugFix

--- web/script/send-order.php
+++ web/script/send-order.php
@@ -8,9 +8,9 @@ foreach (glob("../model/*.php") as $filename)
 \core\core::$configFile = require_once '../config.php';
 $kosikClass = new database\kosik();
 
-    if (is_array($_POST['quntity-1']) AND is_array($_POST['jidlo_id'])){
-        foreach ($_POST['quntity-1'] as $key => $value){
-            $result = $kosikClass->addInKosik($_POST['jidlo_id'][$key], $value);
+    if (is_array($_POST['jidlo_id'])){
+        foreach ($_POST['jidlo_id'] as $key => $value){
+            $result = $kosikClass->addInKosik($_POST['jidlo_id'][$key], 1);
         }
     } else {
         $pocet = $_POST['quntity-1'];
