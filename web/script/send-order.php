22d5b62adacb2ca2b1a07144863d05165ec0d413
TofaVoe <k.kosut@gmail.com>
2018-03-25 21:43:53 +0200

    BugFix

--- web/script/send-order.php
+++ web/script/send-order.php
@@ -10,7 +10,7 @@ $kosikClass = new database\kosik();
 
     if (is_array($_POST['jidlo_id'])){
         foreach ($_POST['jidlo_id'] as $key => $value){
-            $result = $kosikClass->addInKosik($value, 1);
+            if($_POST['count'][$key] != 0)$result = $kosikClass->addInKosik($value, 1);
         }
     } else {
         @$pocet = $_POST['quntity-1'];
