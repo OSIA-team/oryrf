b01ebcb85ec09047c6ab0a40e81e595e6c4abaa2
TofaVoe <k.kosut@gmail.com>
2018-04-05 20:38:34 +0200

    added stav on send order request

--- web/script/send-order.php
+++ web/script/send-order.php
@@ -5,18 +5,22 @@ foreach (glob("../model/*.php") as $filename)
 {
     include $filename;
 }
-\core\core::$configFile = require_once '../config.php';
-$kosikClass = new database\kosik();
-    if (is_array($_POST['jidlo_id'])){
-        foreach ($_POST['jidlo_id'] as $key => $value){
-            $kosikClass->addInKosik($value, 1);
+try{
+    \core\core::$configFile = require_once '../config.php';
+    $kosikClass = new database\kosik();
+        if (is_array($_POST['jidlo_id'])){
+            foreach ($_POST['jidlo_id'] as $key => $value){
+                $kosikClass->addInKosik($value, 1);
+            }
+        } else {
+            @$pocet = $_POST['quntity-1'];
+            $jidlo_id = $_POST['jidlo_id'];
+            $result = $kosikClass->addInKosik($jidlo_id, $pocet);
         }
-    } else {
-        @$pocet = $_POST['quntity-1'];
-        $jidlo_id = $_POST['jidlo_id'];
-        $result = $kosikClass->addInKosik($jidlo_id, $pocet);
-    }
-    $newPocet = $kosikClass->getPocet();
-    $newCena = $kosikClass->getCena();
-    echo json_encode(array("cena" => $newCena, 'pocet' => $newPocet));
+        $newPocet = $kosikClass->getPocet();
+        $newCena = $kosikClass->getCena();
+        echo json_encode(array("stav" => "true", "cena" => $newCena, 'pocet' => $newPocet));
+}  catch (Exception $e) {
+    echo json_encode(array("stav" => "false", "chyba" => $e->getMessage()));
+}
 ?>
