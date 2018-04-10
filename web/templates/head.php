d6b1207bfbc61778b134566c3c1d1db58b07a021
Patriksafar <patrik.safar@educanet.cz>
2018-04-10 23:50:59 +0200

    add script for getting data form analytics

--- web/templates/head.php
+++ web/templates/head.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58092662-5"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-58092662-5');
+    </script>
+
     <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:200,300,400,700" rel="stylesheet">
 
     <!--Import Google Icon Font-->
