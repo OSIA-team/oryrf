1f2bd24dcada1c68b1df414b42f7cc80c1403551
Patriksafar <patrik.safar@educanet.cz>
2018-04-10 20:50:28 +0200

    changed text

--- web/templates/error_page.php
+++ web/templates/error_page.php
@@ -5,7 +5,7 @@
     </div>
     <div class="text">
       <h1>Oooops!</h1>
-      <p>Fritézové servery nezvládli váš požadavek. Ale nezoufejte, mužete to zkusit znovu. Pokud se Vám neustále zobrazuje tato stránka, věřte že na opravě chyby pracujeme.</p>
+      <p>Fritézové servery nezvládly váš požadavek. Ale nezoufejte, mužete to zkusit znovu. Pokud se Vám neustále zobrazuje tato stránka, věřte, že na opravě chyby pracujeme.</p>
       <p>Během čekání bychom vám chtěli nabídnout naše Belgické hranolky. Objednat si je mužete <a href="?page=menu&kategorie=hranolky">zde</a>.</p>
     </div>
   </div>
