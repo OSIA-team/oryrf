64067aff67d67ede5f842cfcf9e3210e5f1556a5
TofaVoe <k.kosut@gmail.com>
2018-06-07 20:47:32 +0200

    oprava kosiku

--- web/script/send-order.php
+++ web/script/send-order.php
@@ -2,6 +2,7 @@
 //var_dump($_POST);
 session_start();
 //pridat_do_kosiku
+var_dump($_POST);
 foreach (glob("../model/*.php") as $filename)
 {
     include $filename;
@@ -9,16 +10,16 @@ foreach (glob("../model/*.php") as $filename)
 try{
     \core\core::$configFile = require_once '../config.php';
     $kosikClass = new database\kosik();
-        if (@isset($_POST['jidlo_id']) AND @is_array($_POST['jidlo_id'])){
+
+        if(isset($_POST['change'])){
+            foreach ($_POST['id'] as $key => $id){
+                $kosikClass->updateInKosik($id, $_POST['pocet'][$key]);
+            }
+        } elseif (@isset($_POST['jidlo_id']) AND @is_array($_POST['jidlo_id'])){
             foreach ($_POST['jidlo_id'] as $key => $value){
                 $kosikClass->addInKosik($value, 1);
             }
-        }   elseif(isset($_POST['change'])) {
-            @$pocet = $_POST['pocet'][0];
-            $jidlo_id = $_POST['jidlo_id'];
-            $result = $kosikClass->updateInKosik($jidlo_id, $pocet);
-        }
-            elseif(@isset($_POST['jidlo_id'])) {
+        } elseif(@isset($_POST['jidlo_id'])) {
             @$pocet = $_POST['quntity-1'];
             $jidlo_id = $_POST['jidlo_id'];
             $result = $kosikClass->addInKosik($jidlo_id, $pocet);
